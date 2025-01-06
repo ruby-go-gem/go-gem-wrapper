9fb149cff66e2fbc9f4826117ace9447471c0fb1
sue445 <sue445@sue445.net>
2025-01-06 22:23:55 +0900

    Add version suffix to generated files

--- _tools/ruby_h_to_go/lib/ruby_h_to_go/type_definition.rb
+++ _tools/ruby_h_to_go/lib/ruby_h_to_go/type_definition.rb
@@ -15,7 +15,7 @@ module RubyHToGo
     # Write definition as go file
     # @param [String] dist_dir
     def write_go_file(dist_dir)
-      go_file_path = File.join(dist_dir, "type_generated.go")
+      go_file_path = File.join(dist_dir, "type_#{GoUtil.ruby_minor_version_build_tag}_generated.go")
 
       GoUtil.generate_initial_go_file(go_file_path)
 
