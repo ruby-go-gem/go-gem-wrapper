1e78dcbd10535883deb94ade5b24c91b2933d6c1
sue445 <sue445@sue445.net>
2025-02-26 00:37:44 +0900

    Impl Config

---
+++ _tools/ruby_h_to_go/lib/ruby_h_to_go/config.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module RubyHToGo
+  class Config
+    def initialize
+      @config = YAML.load(File.read(File.expand_path("../../config.yml", __dir__)))
+    end
+
+    # @return [String]
+    def default_tag
+      @config["default_tag"]
+    end
+
+    # @return [Array<String>]
+    def available_tags
+      @config["available_tags"]
+    end
+  end
+end
